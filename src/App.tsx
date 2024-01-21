9e92ee651afd971e82f78786539af1d529983fd5
jaopredo <joao.profissional.45@gmail.com>
2024-01-21 17:26:49 -0300

    Removendo arquivos desnecessários

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { ConfigContextProvider } from './context/config'
-import { useAppConfig } from './Configs'
-
-import SchemaForm from './SchemaForm'
 
 export default function App() {
-    const config = useAppConfig()
-
     return <div className="App">
-        <ConfigContextProvider config={config}>
-            <SchemaForm/>
-        </ConfigContextProvider>
     </div>
 }
