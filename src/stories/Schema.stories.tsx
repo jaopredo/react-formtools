7edda75744f75d02db85591fb3a97cc6b2554c5d
jaopredo <joao.profissional.45@gmail.com>
2024-07-05 16:05:50 -0300

    Refazedo os stories

---
+++ src/stories/Schema.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+ 
+import { FormtoolsSchema, FormtoolsForm } from '..'
+import type { SchemaType, SchemaProps } from '..'
+ 
+const meta: Meta<SchemaProps> = {
+    component: FormtoolsSchema,
+    decorators: (Story) => <FormtoolsForm onSubmit={()=>{}}>
+        <Story/>
+    </FormtoolsForm>
+}
+ 
+export default meta
+type Story = StoryObj<SchemaProps>
+ 
+const schema: SchemaType[] = [
+    {
+        formtool: 'text',
+        label: 'Teste',
+        name: 'test'
+    }
+]
+
+export const Primary: Story = {
+    args: {
+        schema
+    },
+}
