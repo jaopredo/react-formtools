67d5b44e0a60701c5ebeabe25531482140e59081
João Pedro <joao.profissional.45@gmail.com>
2024-12-09 19:43:38 -0300

    Documentação atualizada e alguns stories criados

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,23 +3,23 @@ import type { FormtoolsFormProps } from '../types/form'
 import React, { useEffect } from 'react'
 
 export function FormtoolsForm<T extends FieldValues>({ children, onSubmit, setMethods, multipart, className }: FormtoolsFormProps<T>) {
-	const methods = useForm<T>()
+	const methods = useForm<T>()  // Retrieving the methdos from useForm values
 
 	useEffect(() => {
-		if (setMethods) {
+		if (setMethods) {  // If the user passed a setState for setMethods, I'll pass it back to him
 			setMethods(methods)
 		}
 	}, [])
 
-	const beforeSubmit: SubmitHandler<T> = (data) => {
-		if (multipart) {
+	const beforeSubmit: SubmitHandler<T> = (data) => {  // Function executed before submiting the form
+		if (multipart) {  // If it is a multipartform
 			const formdata = new FormData
 			Object.keys(data).forEach(field => {
 				formdata.append(field, data[field])
 			})
-			onSubmit(formdata)
-		} else {
-			onSubmit(data)
+			onSubmit(formdata)  // Pass a FormData as parameter from the passed function
+		} else {  // Else
+			onSubmit(data)  // Pass the JSON data
 		}
 	}
 
